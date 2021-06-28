e7cec9cff96bb2fa5f79dd2b46dde0501df9320a
Allan Hardy <al_hardy@live.com.au>
2021-06-29 00:46:36 +1000

    adding assume role to examples for dev account

--- examples/with-flow-logs/providers.tf
+++ examples/with-flow-logs/providers.tf
@@ -1,5 +1,9 @@
 provider "aws" {
   region = "ap-southeast-2"
+  assume_role {
+    role_arn = "arn:aws:iam::171101346296:role/DeveloperAccessRole"
+    session_name = "terraform"
+  }
 }
 
 terraform {
